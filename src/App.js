51dd0d2181b42404863ab5dc9be461163453c3ff
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-02-07 22:42:25 +0000

    Added new pages
    Added 4 new pages, using class- and functional-based components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import React from 'react';
 // import './App.css';
+import Home from './pages/Home.js';
+import Rooms from './pages/Rooms.js';
+import SingleRoom from './pages/SingleRoom.js';
+import Error from './pages/Error.js';
 
 function App() {
-  return <>hello</>;
+  return <>
+  <Home/>
+  <Rooms/>
+  <SingleRoom/>
+  <Error/>
+  </>;
 }
 
 export default App;
