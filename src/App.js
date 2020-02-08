a3b1e35b124a36723278f667af1bc0de11832a3b
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-02-08 15:33:37 +0000

    4 different components imported to App.js
    I eventually opened 4 js files/ components (for room and error pages), and imported the files to the main component (App.js). It is easier and cleaner to manage the page components, that way.

--- src/App.js
+++ src/App.js
@@ -1,39 +1,18 @@
 import React from 'react';
-import './App.css';
-import {BrowserRouter as Router} from 'react-router-dom';
-import Route from 'react-router-dom/Route';
+//import './App.css';
 
-
+import Home from './pages/Home';
+import {Rooms} from './pages/Rooms';
+import SingleRooms from './pages/SingleRooms';
+import Error from './pages/Error';
 
 function App() {
-  return (
-     <Router>
-        <Route path="/" exact strict render={
-         ()=> {
-            return ( <h1>Welcome home</h1>);
-         }
-        }/>
-
-        <Route path="/Rooms" exact strict render={
-         ()=> {
-            return ( <h1>Check the rooms</h1>);
-         }
-        }/>
-
-        <Route path="/SingleRooms" exact strict render={
-
-         ()=> {
-            return ( <h1>Check our single rooms</h1>);
-         }
-        }/>
-
-        <Route path="/Error" exact strict render={
-         ()=> {
-            return ( <h1>Error Page</h1>);
-         }
-        }/>
-     </Router>
-  );
-}
+  return <>
+  <Home/>
+  <Rooms/>  
+  <SingleRooms/>  
+  <Error/>
+  </>;
+};
 
 export default App;
