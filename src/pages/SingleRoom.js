9aef64ede1d3b760742d52d96c188268d6445918
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-02-08 15:27:29 +0000

    Updated

--- src/pages/SingleRoom.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from './node_modules/react'
-
-export default class SingleRoom extends 
-Component {
-    render() {
-        return <div>hello from single room page</div>;
-        
-    }
-}
---
+++ src/pages/SingleRooms.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class SingleRooms extends Component {
+    render() {
+        return (
+            <div>
+                Single rooms
+            </div>
+        );
+    }
+}
