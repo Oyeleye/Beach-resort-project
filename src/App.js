f218d27e1730d1af8876ef4419baee8d23413808
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-02-08 11:14:08 +0000

    Compiled the rooms and error pages on same file
    Instead of having 4 different files for the rooms and error pages, I created different paths for the pages on app.js, having imported BrowserRouter as Router from react router dom, and also imported Route from 'react-router-dom/Route'.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,39 @@
 import React from 'react';
-// import './App.css';
-import Home from './pages/Home.js';
-import Rooms from './pages/Rooms.js';
-import SingleRoom from './pages/SingleRoom.js';
-import Error from './pages/Error.js';
+import './App.css';
+import {BrowserRouter as Router} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
+
+
 
 function App() {
-  return <>
-  <Home/>
-  <Rooms/>
-  <SingleRoom/>
-  <Error/>
-  </>;
+  return (
+     <Router>
+        <Route path="/" exact strict render={
+         ()=> {
+            return ( <h1>Welcome home</h1>);
+         }
+        }/>
+
+        <Route path="/Rooms" exact strict render={
+         ()=> {
+            return ( <h1>Check the rooms</h1>);
+         }
+        }/>
+
+        <Route path="/SingleRooms" exact strict render={
+
+         ()=> {
+            return ( <h1>Check our single rooms</h1>);
+         }
+        }/>
+
+        <Route path="/Error" exact strict render={
+         ()=> {
+            return ( <h1>Error Page</h1>);
+         }
+        }/>
+     </Router>
+  );
 }
 
 export default App;
