dfcb9c7bc18bfeaa049e55473e61918765717386
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-02-10 20:08:51 +0000

    import Banner and Links

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import Hero from '../components/Hero';
-
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
 
 function Home() {
-  return <Hero />;
+  return (
+      <>
+         <Hero>
+           <Banner title='luxurious rooms' subtitle='deluxe rooms starting at $299'>
+              <Link to='/rooms' className='btn-primary'>
+                our rooms
+              </Link>
+           </Banner>
+         </Hero>;
+      </>
+      );
 }
 
 export default Home;
--- src/pages/Rooms.js
+++ src/pages/Rooms.js
@@ -1,6 +1,15 @@
 import React from 'react'
 import Hero from '../components/Hero';
+import Banner from '../components/Banner';
+import { Link } from 'react-router-dom';
+
 
 export const Rooms = () => {
-    return <Hero hero= 'roomsHero'/>
+    return (
+    <Hero hero= 'roomsHero'> 
+       <Banner title='our rooms'>
+           <Link to='/' className='btn-primary'>return home</Link>
+       </Banner>
+    </Hero>
+    );
 }
