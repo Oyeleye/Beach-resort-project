f6a329f64a0df28fa3148f7a06b683feb73ee3f0
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-02-10 18:41:27 +0000

    add 2 props and set default for use in App.css

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -3,3 +3,7 @@ import React from 'react';
 export default function Hero( {children, hero } ) {
     return <header className={hero}>{children}</header>;   
 }
+ 
+Hero.defaultProps = {
+    hero: 'defaultHero'
+};
