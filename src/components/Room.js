b30d53fa32ae01fa0038730a0da942cb1be7d13d
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-02-12 21:24:07 +0000

    create Room, import Link and defaultImg

---
+++ src/components/Room.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import defaultImg from '../images/guessingGame.jpg';
+
+export default function Room( { room }) {
+    const{name, slug, images, price} = room;
+
+    return (
+        <article className='room'>
+            <div className='img-container'>
+               <img src={images[0] || defaultImg} alt='single room'/>
+            </div> 
+        </article>
+    )
+}
