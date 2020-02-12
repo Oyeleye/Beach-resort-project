4a1151039de145b9b327cf6abd0cdc8f2e7734fc
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-02-12 17:01:33 +0000

    create FeaturedRooms and import RoomContext from Context

---
+++ src/components/FeaturedRooms.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'
+import {RoomContext} from '../Context';
+
+export default class FeaturedRooms extends 
+Component {
+   static contextType = RoomContext;
+       render() {
+const {featuredRooms : rooms} = this.context;
+console.log(rooms);
+           return (
+             <div>
+                from Lagos
+                </div>
+           );   
+    }
+}
