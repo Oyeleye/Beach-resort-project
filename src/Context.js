0a7152c28f6b61aa2ac15ae88041a65ae8ec7f5e
Oyeleye Oyedayo <44131391+Oyeleye@users.noreply.github.com>
2020-02-12 18:08:59 +0000

    tate room, sortedRooms, featuredRooms and loading

--- src/Context.js
+++ src/Context.js
@@ -11,7 +11,7 @@ class RoomProvider extends Component {
         featuredRooms:[],
         loading: true
     };
-    //getDataa
+    //getData
 
     componentDidMount(){
         let rooms = this.formatData(items);
